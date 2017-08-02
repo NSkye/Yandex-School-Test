dd7fc949c7030f50751632846aa8ecfd0f7f5bff
Nikolai-Laevskii <30626748+Nikolai-Laevskii@users.noreply.github.com>
2017-08-02 10:53:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let MyForm = {
 	        email = Data.email,
 	        /*ВАЛИДАЦИЯ ФИО*/
         	checkFio = / *(([А-я]+([\'\’\-‎]?[А-я]+)*)+ +){2}(([А-я]+([\'\’\-‎]?[А-я]+)*)+ *)/g,
-            notValidFio = !fio.match(checkFio) || fio !== fio.match(checkFio)[0],
+            	notValidFio = !fio.match(checkFio) || fio !== fio.match(checkFio)[0],
 	        /*ВАЛИДАЦИЯ ПОЧТЫ*/
 	        checkEmail = /[A-z]+\d*([\.\-]?[A-z\d]+)*@(ya.ru|yandex.ru|yandex.ua|yandex.by|yandex.kz|yandex.com)/g,
 	        notValidEmail = !email.match(checkEmail) || email !== email.match(checkEmail)[0] || (email.split('@')[0].length > 30),
